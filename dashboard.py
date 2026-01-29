5bdcd9be7e3d34bced81e5c13170aaf9a102d288
ykost <ykostenko30@gmail.com>
2026-01-28 22:41:01 -0500

    Update requirements and files

---
+++ dashboard.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# in the terminal run: streamlit run dashboard.py
+# if prompts you to enter email, just press enter/run
+# if streamlit not recognized, run: python -m streamlit run dashboard.py
+
+st.title("Water Quality Dashboard")
+st.header("Internship Ready Software Development")
+st.subheader("Yana Kostenko")
+st.divider()
+
+df = pd.read_csv("biscayneBay_waterquality.csv")
+
+tab1, tab2, tab3, tab4 = st.tabs(
+    ["Descriptive Statistics",
+     "2d Plots",
+     "3d Plots",
+     "More"]
+)
+
+with tab1:
+    st.dataframe(df)
+    st.caption("Raw Data")
+    st.divider()
+    st.dataframe(df.describe())
+    st.caption("Descriptive Statistics")
+
+with tab2:
+    fig1 = px.line(df,
+                   x="Time",
+                   y="Temperature (c)")
+    st.plotly_chart(fig1)
+
+    fig2 = px.scatter(df,
+                      x="ODO mg/L",
+                      y="Temperature (c)",
+                      color="pH")
+    st.plotly_chart(fig2)
+
+with tab3:
+    fig3 = px.scatter_3d(df,
+                         x="Longitude",
+                         y="Latitude",
+                         z="Total Water Column (m)",
+                         color="Temperature (c)")
+    fig3.update_scenes(zaxis_autorange="reversed")
+    st.plotly_chart(fig3)
+
+with tab4:
+    st.warning("More coming soon")
